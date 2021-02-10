ba8ebe3f5f1a6618c5474a61e98d8b22213c320b
William Baker <willbaker1988@hotmail.co.uk>
2021-02-10 15:10:25 +0200

    created local setup for development database

--- local_setup/setup.py
+++ local_setup/setup.py
@@ -1,35 +1,96 @@
 import json
 import os
-path = os.path.abspath(os.sep)
-# print(path)
+import logging
+import snowflake.connector
+from snowflake.connector.errors import DatabaseError
+
+# create log file
+logging.basicConfig(level=logging.INFO, filename='app.log', filemode='w', format='%(asctime)s -%(name)s - %(levelname)s - %(message)s')
+#logging.warning('This will get logged to a file')
+
 dir_path = os.path.dirname(os.path.realpath(__file__))
-
 with open(dir_path + os.sep + 'flyway_config.json') as config_file:
-    data = json.load(config_file)
+    environ = json.load(config_file)
 
+dir_path = dir_path.replace("local_setup","")
 
-snowflake_user = data['snowflake_user']
-snowflake_password = data['snowflake_password']
-snowflake_role = data['snowflake_role']
-cloneable_snowflake_database = snowflake_user + '_DEV'
-url = "flyway.url=jdbc:snowflake://bigdatasolutions.west-europe.azure.snowflakecomputing.com/?db='{}'&warehouse=LOAD_WH&role='{}'&authenticator=snowflake".format(cloneable_snowflake_database,snowflake_role)
-clone_statement = "CREATE DATABASE {} CLONE DATABASE SURVEY".format(cloneable_snowflake_database)
+SNOWFLAKE_USER = environ['SF_USER']
+SNOWFLAKE_PWD = environ['SF_PWD']
+SNOWFLAKE_ROLE = environ['SF_ROLE']
+SNOWFLAKE_WH = environ['SF_WH']
+SNOWFLAKE_ACCOUNT = "bigdatasolutions.west-europe.azure"
+SNOWFLAKE_DATABASE = "SURVEY_PROD"
+SNOWFLAKE_SCHEMA = "PUBLIC"
+CONCAT = "SNOWFLAKE_USER: {}/n SNOWFLAKE_PWD: {}/n".format(
+    SNOWFLAKE_USER,
+    SNOWFLAKE_PWD)
+
+CLONE_DEV_DATABASE = SNOWFLAKE_USER + '_DEV'
+
+URL = "flyway.url=jdbc:snowflake://{}.com/?db={}&warehouse={}&role={}&authenticator=snowflake".format(
+            SNOWFLAKE_ACCOUNT,
+            CLONE_DEV_DATABASE,
+            SNOWFLAKE_WH,
+            SNOWFLAKE_ROLE
+        )
 
 flyway_conf = """
 # Long properties can be split over multiple lines by ending each line with a backslash
 flyway.locations=filesystem:databases/survey_demo
 flyway.user={}
 flyway.password={}
+flyway.schema={}
 flyway.url={}
 # flyway.url=jdbc:snowflake://bigdatasolutions.west-europe.azure.snowflakecomputing.com/?db=SURVEY_DEV&warehouse=LOAD_WH&role=SYSADMIN&authenticator=snowflake
+""".format(SNOWFLAKE_USER,SNOWFLAKE_PWD,SNOWFLAKE_SCHEMA,URL)
 
-""".format(snowflake_user,snowflake_password,url)
+# Auth to create Snowflake Cursor.
+def auth():
+    ctx = snowflake.connector.connect(
+        user=SNOWFLAKE_USER,
+        role=SNOWFLAKE_ROLE,
+        password=SNOWFLAKE_PWD,
+        account=SNOWFLAKE_ACCOUNT,
+        warehouse=SNOWFLAKE_WH, 
+        database=SNOWFLAKE_DATABASE,
+        schema=SNOWFLAKE_SCHEMA
+        )
+    return ctx.cursor()
 
-#f.
-open(path + "flyway.conf_test","w+")
-#write.file("flyway.conf_test")
+def create_flyway_conf(path, filename, contents):
+    f = open(dir_path + filename,"w+")
+    f.write(contents)
+    f.close()
 
-print(flyway_conf)
+
+"""
+f = open(dir_path + "flyway.conf","w+")
+f.write(flyway_conf)
+f.close() 
+"""
+
+
+def main():
+    logging.info("creating flyway.conf")
+    logging.info("variables set are %s", CONCAT)
+    conf = create_flyway_conf(path=dir_path,filename="flyway.conf",contents=flyway_conf)
+    if os.path.isfile(dir_path + 'flyway.conf') == True:
+        logging.info("created flyway.conf file")
+        logging.info("preparing local development database  %s.", CLONE_DEV_DATABASE)
+        cs=auth()
+        try:
+            sql = "DROP DATABASE IF EXISTS {};".format(CLONE_DEV_DATABASE)
+            cs.execute(sql)
+            logging.info("queryid %s", cs.sfqid)
+            sql = "CREATE DATABASE {} CLONE {};".format(CLONE_DEV_DATABASE,SNOWFLAKE_DATABASE)
+            cs.execute(sql)
+            logging.info("queryid %s", cs.sfqid)
+        finally:
+            cs.close()
+    else:
+        logging.error("flyway.conf file hasn't been created in root")
+            
+main()
 
 
 
